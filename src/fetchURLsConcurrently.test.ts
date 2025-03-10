8c103768dd1e6c145d74e1a12ee1a8d3b4f923ae
shnyder <ajs.private@gmail.com>
2025-03-10 16:26:12 +0100

    added tests and a more convenient interface

---
+++ src/fetchURLsConcurrently.test.ts
@@ -0,0 +1,42 @@
+import { fetchURLsConcurrently } from "./fetchURLsConcurrently";
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ thisIsAMockProperty: "on a mock Response object" }),
+  }),
+) as jest.Mock;
+
+test("Should fetch smaller number of URLs from server", async () => {
+  const input = [
+    "http://schema.org/Thing",
+    "http://schema.org/Person",
+    "http://schema.org/Product",
+  ].map((val) => new URL(val));
+  //this one
+  const actual = await fetchURLsConcurrently(input);
+  expect(actual.length).toBe(input.length);
+});
+
+test("Should fetch larger number of URLs from server", async () => {
+  const input = [
+    "http://schema.org/Thing",
+    "http://schema.org/Person",
+    "http://schema.org/Product",
+		"http://schema.org/Action",
+    "http://schema.org/Event",
+    "http://schema.org/Intangible",
+		"http://schema.org/MedicalEntity",
+    "http://schema.org/Organization",
+    "http://schema.org/Place",
+		"http://schema.org/Taxon",
+    "http://schema.org/CreativeWork",
+    "http://schema.org/Blog",
+		"http://schema.org/Conversation",
+    "http://schema.org/Episode",
+    "http://schema.org/Movie",
+  ].map((val) => new URL(val));
+  //this one
+  const actual = await fetchURLsConcurrently(input);
+	console.log(actual)
+  expect(actual.length).toBe(input.length);
+});
--- src/fetchURLsConcurrently.ts
+++ src/fetchURLsConcurrently.ts
@@ -1,22 +1,34 @@
 const MAX_CONCURRENCY: number = 14;
 
+/**
+ * the response url is the resolved URL on the server.
+ * Code that requests this function would most likely want to match the result
+ * to a request
+ */
+type FulfilledRequest = {
+  requestedURL: URL,
+  response: Response
+}
+
 export async function fetchURLsConcurrently(
   requestedURLs: URL[],
   maxConcurrentRequest: number = MAX_CONCURRENCY
-): Promise<Response[]> {
+): Promise<FulfilledRequest[]> {
   let nextURLidx: number = 0;
   const internalRequestCount =
     requestedURLs.length < maxConcurrentRequest
       ? requestedURLs.length
       : maxConcurrentRequest;
-  const appendingNextRequestsWrapperFn = (): Promise<Response[]> => {
-    const wrapperResult = new Promise<Response[]>(async (resolve, reject) => {
-      const executorResult: Response[] = []
+  const appendingNextRequestsWrapperFn = (): Promise<FulfilledRequest[]> => {
+    const wrapperResult = new Promise<FulfilledRequest[]>(async (resolve, reject) => {
+      const executorResult: FulfilledRequest[] = []
       let nextUrl: URL | null = requestedURLs[nextURLidx];
       do {
-        const [response, _respNextUrl] = await executeRequestFn(nextUrl);
-        executorResult.push(response);
-        nextUrl = _respNextUrl
+        const [response, _nextUrlAfterResponse] = await executeRequestFn(nextUrl);
+        executorResult.push({
+          requestedURL: nextUrl,
+          response: response});
+        nextUrl = _nextUrlAfterResponse
       } while (!!nextUrl);
       resolve(executorResult);
     });
@@ -25,18 +37,18 @@ export async function fetchURLsConcurrently(
   const executeRequestFn = (url: URL): Promise<[Response, URL | null]> => {
     return fetch(url).then((response) => {
       const nextUrl =
-        nextURLidx < internalRequestCount ? requestedURLs[nextURLidx] : null;
+        nextURLidx <= internalRequestCount ? requestedURLs[nextURLidx] : null;
       nextURLidx++;
       return [response, nextUrl];
     });
   };
 
-  const concurrentPromises: Promise<Response[]>[] = [];
+  const concurrentPromises: Promise<FulfilledRequest[]>[] = [];
   for (let i = 0; i < internalRequestCount; i++) {
     concurrentPromises.push(appendingNextRequestsWrapperFn());
     nextURLidx++;
   }
-  const result: Response[] = await Promise.all(concurrentPromises).then(
+  const result: FulfilledRequest[] = await Promise.all(concurrentPromises).then(
     (allResolvedRequests) => {
       //joins the Result arrays
       return allResolvedRequests.flat();
